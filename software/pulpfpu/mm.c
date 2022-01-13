e9ab1e2f4e0d9d8569d8b0bc7da832578a1a732a
Luca Valente <luca.valente2@unibo.it>
2022-01-13 12:30:40 +0100

    Tets div as well.

--- software/pulpfpu/mm.c
+++ software/pulpfpu/mm.c
@@ -18,19 +18,23 @@ int thread_entry(int cid, int nc) {
   float fv0[8] = { 29.0024052F, 16.8149376F, 42.0270653F, 86.9266739F, 167475.188F, 167471.844F, 738649.812F,  738635.062F  };
   float fv1[8] = { 86825.1797F, 86823.4453F, 257317.766F, 257312.609F, 205468.344F, 205464.25F, 1.28340175E+6F, 1.283376E+6F };
   float * fv2 ;
+  float * fv3 ;
   float * fresult ;
   int * result_pointer;
   float gold_result = 1964788416512.0F;
 
   i = 0x10001030;
   fv2 = 0x10000000 ;
+  fv3 = 0x10000040 ;
   fresult = 0x10001020 ;
   result_pointer = 0x10001020;
   
   for (int k=0; k<8; k++)
-    if(core_id()==k)
+    if(core_id()==k) {
       fv2[k] = fv0[k] * fv1[k];
-
+      fv3[k] = fv0[k] / fv1[k];
+    }
+  
   if(core_id()==7){
       pulp_write32(i,8);
   }
@@ -39,12 +43,17 @@ int thread_entry(int cid, int nc) {
 
     while(pulp_read32(i)!=8);
     fresult[0]=0.0F;
-    for (int j=0; j<8; j++)
+    fresult[1]=0.0F;
+    
+    for (int j=0; j<8; j++) {
       fresult[0] = fresult[0] + fv2[j];
-
+      fresult[1] = fresult[1] + fv3[j];
+    }
     
     printf("%x\n", *result_pointer);
-    if(*result_pointer!=0x53e4bb42)
+    printf("%x\n", *(result_pointer+0x1));
+    
+    if( (*result_pointer!=0x53e4bb42) || (*(result_pointer+0x1)!=4032110a) )
       printf("error\n");
     pulp_write32(0x10001000,1);
 
