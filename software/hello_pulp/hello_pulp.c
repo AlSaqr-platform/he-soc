13126d4954fb6ee15481017b6a96cbad8848f8d3
Yvan Tortorella <ytortorella@lagrev1.ee.ethz.ch>
2022-03-23 00:45:33 +0100

    Changed PULP and memory tests Makefiles

--- software/hello_pulp/hello_pulp.c
+++ software/hello_pulp/hello_pulp.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include "utils.h"
 #include "./cluster_code.h"
-//#define FPGA_EMULATION
+// #define FPGA_EMULATION
 
 int main(int argc, char const *argv[]) {
 
   #ifdef FPGA_EMULATION
-  int baud_rate = 9600;
-  int test_freq = 10000000;
+  int baud_rate = 115200;
+  int test_freq = 50000000;
   #else
   set_flls();
   int baud_rate = 115200;
@@ -20,9 +20,9 @@ int main(int argc, char const *argv[]) {
   tlb_cfg(H2C_TLB_BASE_ADDR, 0, h2c_first_va, h2c_last_va, h2c_base_pa, h2c_flags);
   // C2H TLB configuration
   tlb_cfg(C2H_TLB_BASE_ADDR, 0, c2h_first_va, c2h_last_va, c2h_base_pa, c2h_flags);
-  uint32_t * hyaxicfg_reg_mask = 0x1A104018;
+  uint32_t * hyaxicfg_reg_mask = 0x1A101018;
   pulp_write32(hyaxicfg_reg_mask,26); //128MB addressable
-  uint32_t * hyaxicfg_reg_memspace = 0x1A104024;
+  uint32_t * hyaxicfg_reg_memspace = 0x1A101024;
   pulp_write32(hyaxicfg_reg_memspace,0x84000000); // Changing RAM end address, 64 MB
   // cluster setup
   load_cluster_code();
