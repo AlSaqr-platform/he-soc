0ecc21db50a828f289ccf17cf7a1a4ec4437521c
Maicol Ciani <mciani@lagrev5.ee.ethz.ch>
2022-04-12 20:19:03 +0200

    rewrote the test code for scmi in a better way

--- software/hello/hello.c
+++ software/hello/hello.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
-//#define FPGA_EMULATION
+//#define N 20
+
+//const int RESULT_FIB[N] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181};
 
 int main(int argc, char const *argv[]) {
-
   #ifdef FPGA_EMULATION
   int baud_rate = 9600;
   int test_freq = 10000000;
@@ -13,18 +14,46 @@ int main(int argc, char const *argv[]) {
   set_flls();
   int baud_rate = 115200;
   int test_freq = 100000000;
-  #endif  
+  #endif
+  
   uart_set_cfg(0,(test_freq/baud_rate)>>4);
-  uint32_t * hyaxicfg_reg_mask = 0x1A101018;
-  pulp_write32(hyaxicfg_reg_mask,26); //128MB addressable
-  uint32_t * hyaxicfg_reg_memspace_end_addr1 = 0x1A10102C;
-  pulp_write32(hyaxicfg_reg_memspace_end_addr1,0x88000000);
-  uint32_t * hyaxicfg_reg_memspace_start_addr1 = 0x1A101028;
-  pulp_write32(hyaxicfg_reg_memspace_start_addr1,0x84000000);
-  uint32_t * hyaxicfg_reg_memspace_end_addr0 = 0x1A101024;
-  pulp_write32(hyaxicfg_reg_memspace_end_addr0,0x84000000); 
-  printf("Hello CVA6!\n");
+ 
+  int volatile * p_reg, * p_reg1, * p_reg2, * p_reg3, * p_reg4, * p_reg5;
+  int a, b, c, d, e;
+  
+  p_reg  = (int *) 0x50000004;
+  
+  p_reg1 = (int *) 0x50000008;
+  p_reg2 = (int *) 0x50000010;
+  p_reg3 = (int *) 0x50000014;
+  p_reg4 = (int *) 0x50000018;
+  p_reg5 = (int *) 0x5000001C;
+
+  *p_reg1 = 0xBAADC0DE;
+  *p_reg2 = 0xBAADC0DE;
+  *p_reg3 = 0xBAADC0DE;
+  *p_reg4 = 0xBAADC0DE;
+  *p_reg5 = 0xBAADC0DE;
+
+  a = *p_reg1;
+  b = *p_reg2;
+  c = *p_reg3;
+  d = *p_reg4;
+  e = *p_reg5; 
+
+  if( a == 0xBAADC0DE && b == 0xBAADC0DE && c == 0xBAADC0DE && d == 0xBAADC0DE && e == 0xBAADC0DE){
+     *p_reg = 0x00000001;
+     while(*p_reg != 0x00000000);
+  }
+  else{
+     printf("Test failed!\n");
+     uart_wait_tx_done();
+     return 0;
+  }
+
+  printf("Test succeeded!\n");
   uart_wait_tx_done();
+  
   return 0;
 }
  
