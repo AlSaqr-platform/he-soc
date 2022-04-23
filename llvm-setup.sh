0fe4e0ef915910a78145ae982b29ee858f19ac6f
Yvan Tortorella <ytortorella@lagrev1.ee.ethz.ch>
2022-04-23 13:04:57 +0200

    Distinguished GCC and LLVM toolchains in setup scripts.

--- llvm-setup.sh
+++ llvm-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "exporting RISCV"
 
-export PATH=/usr/scratch/lagrev5/lvalente/riscv_install/bin:$PATH
-
 export RISCV=/usr/scratch/lagrev5/lvalente/riscv_install
+
+echo "exporting CLANG"
+
+export PATH=/usr/scratch/lagrev1/ytortorella/hero-linux-toolchain/install/bin:$PATH
 
 export ROOT_DIR=$(pwd)
 
@@ -11,17 +13,11 @@ export SW_HOME=$(pwd)/software
 
 export HW_HOME=$(pwd)/hardware
 
+export TOOLCHAIN=LLVM
+
 echo "exporting QUESTASIM PATH"
 
 export QUESTASIM_HOME=/usr/pack/modelsim-10.7b-kgf/questasim/
-
-echo "exporting RISCV 32 bit"
-
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
-
-echo "exporting RISCV 32 bit with zfinx"
-
-export PATH=/usr/pack/riscv-1.0-kgf/pulp-gcc-2.5.0-rc1/bin:$PATH
 
 echo "cloning submodules"
 
