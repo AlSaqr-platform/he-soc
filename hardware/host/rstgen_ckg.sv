33d91eec112d281ba23430bade45e54c572df35e
Mattia Sinigaglia <mattia.sinigaglia5@unibo.it>
2023-10-13 17:59:32 +0200

    add local resetgen with output clock

---
+++ hardware/host/rstgen_ckg.sv
@@ -0,0 +1,32 @@
+// Copyright 2018 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+// Davide Rossi <davide.rossi@unibo.it>
+
+module rstgen_ckg (
+    input  logic clk_i,
+    input  logic rst_ni,
+    input  logic test_mode_i,
+    output logic rst_no,
+    output logic init_no,
+    output logic clk_o
+);
+
+    rstgen_bypass_ckg i_rstgen_bypass_ckg (
+        .clk_i            ( clk_i       ),
+        .rst_ni           ( rst_ni      ),
+        .rst_test_mode_ni ( rst_ni      ),
+        .test_mode_i      ( test_mode_i ),
+        .rst_no           ( rst_no      ),
+        .init_no          ( init_no     ),
+        .clk_o            ( clk_o       )
+    );
+
+endmodule
