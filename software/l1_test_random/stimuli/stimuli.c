603d4632973bea55b3fd57c602f925f511dc87c8
Riccardo Tedeschi <riccardo.tedeschi4@studio.unibo.it>
2023-10-17 21:41:38 +0200

    Adapt L1 test to parallel execution

--- software/l1_test_random/stimuli/stimuli.c
+++ software/l1_test_random/stimuli/stimuli.c
@@ -12,7 +12,7 @@
 #endif
 
 #define BASE_ADDR     0x10000000
-#define MEM_SIZE      64 * 1024
+#define MEM_SIZE      256 * 1024
 
 #ifdef PSEUDO_RANDOM_VALUES
 
@@ -30,11 +30,11 @@
   #define TEST_VALUE    0xcafecafe           // write/read test value
 #endif
 
-void check_write_read_l2(int*);
+void check_write_read_l1(int*);
+
+PI_L2 int retval[8];
 
 int main () {
-
-  int retval = -1;
 
   synch_barrier();
   
@@ -42,13 +42,16 @@ int main () {
   // TEST //
   //////////
 
-  check_write_read_l2(&retval);
+  check_write_read_l1(retval);
 
   synch_barrier();
 
   if(core_id() == 0){
+    int acc = 0;
+    for (int i = 0; i < 8; i++)
+      acc += retval[i];
     // Write msg to mailbox
-    pulp_write32(0x10403000, retval);
+    pulp_write32(0x10403000, acc);
   }
 
   while (1) {
@@ -58,77 +61,73 @@ int main () {
   return 0;
 }
 
-void check_write_read_l2 (int *retval) {
+void check_write_read_l1 (int *retval) {
 
-  if (core_id() == 0) {
+  unsigned int errors = 0;
 
-    unsigned int errors = 0;
+  #ifdef VERBOSE
+  printf("L1 WRITE READ TEST\n");
+  #endif
 
-    #ifdef VERBOSE
-    printf("L2 WRITE READ TEST\n");
-    #endif
+  #ifdef PSEUDO_RANDOM_VALUES
+    uint32_t xorshift_state = DEFAULT_SEED * (1 + core_id());
+  #endif
+  int *current_addr = (int *) BASE_ADDR + core_id();
 
+  #ifdef VERBOSE
+  printf("=== WRITE ===\n");
+  printf("First addr written: %p\n", current_addr);
+  #endif
+
+  for (unsigned int i = 0; i < (MEM_SIZE >> 5); i++) {
     #ifdef PSEUDO_RANDOM_VALUES
-      uint32_t xorshift_state = DEFAULT_SEED;
+      xorshift_state = xorshift32(xorshift_state);
+      (*current_addr) = xorshift_state;
+    #else
+      (*current_addr) = TEST_VALUE;
     #endif
-    int *current_addr = (int *) BASE_ADDR;
-
-    #ifdef VERBOSE
-    printf("=== WRITE ===\n");
-    printf("First addr written: %p\n", current_addr);
-    #endif
-
-    for (unsigned int i = 0; i < (MEM_SIZE >> 2); i++) {
-      #ifdef PSEUDO_RANDOM_VALUES
-        xorshift_state = xorshift32(xorshift_state);
-        (*current_addr) = xorshift_state;
-      #else
-        (*current_addr) = TEST_VALUE;
-      #endif
-      current_addr++;
-    }
-
-    #ifdef VERBOSE  
-    printf("Last addr written: %p\n", (current_addr-1));
-    #endif
-
-    #ifdef PSEUDO_RANDOM_VALUES
-      xorshift_state = DEFAULT_SEED;
-    #endif
-    current_addr = (int *) BASE_ADDR;
-
-    #ifdef VERBOSE
-    printf("=== READ ===\n");
-    printf("First addr read: %p\n", current_addr);
-    #endif
-
-    for (unsigned int i = 0; i < (MEM_SIZE >> 2); i++) {
-      #ifdef PSEUDO_RANDOM_VALUES
-        xorshift_state = xorshift32(xorshift_state);
-        if (*current_addr != xorshift_state) {
-          #ifdef VERBOSE
-          printf("@%p: expected: %x, actual: %x\n", current_addr, xorshift_state, *current_addr);
-          #endif
-          errors++;
-        }
-      #else
-        if (*current_addr != TEST_VALUE) {
-          #ifdef VERBOSE
-          printf("@%p: expected: %x, actual: %x\n", current_addr, TEST_VALUE, *current_addr);
-          #endif
-          errors++;
-        }
-      #endif
-      current_addr++;
-    }
-
-    #ifdef VERBOSE
-    printf("Last addr read: %p\n", (current_addr-1));
-    #endif
-
-    *retval = errors;
-
+    current_addr += 8;
   }
+
+  #ifdef VERBOSE  
+  printf("Last addr written: %p\n", (current_addr-1));
+  #endif
+
+  #ifdef PSEUDO_RANDOM_VALUES
+    xorshift_state = DEFAULT_SEED * (1 + core_id());
+  #endif
+  current_addr = (int *) BASE_ADDR + core_id();
+
+  #ifdef VERBOSE
+  printf("=== READ ===\n");
+  printf("First addr read: %p\n", current_addr);
+  #endif
+
+  for (unsigned int i = 0; i < (MEM_SIZE >> 5); i++) {
+    #ifdef PSEUDO_RANDOM_VALUES
+      xorshift_state = xorshift32(xorshift_state);
+      if (*current_addr != xorshift_state) {
+        #ifdef VERBOSE
+        printf("@%p: expected: %x, actual: %x\n", current_addr, xorshift_state, *current_addr);
+        #endif
+        errors++;
+      }
+    #else
+      if (*current_addr != TEST_VALUE) {
+        #ifdef VERBOSE
+        printf("@%p: expected: %x, actual: %x\n", current_addr, TEST_VALUE, *current_addr);
+        #endif
+        errors++;
+      }
+    #endif
+    current_addr += 8;
+  }
+
+  #ifdef VERBOSE
+  printf("Last addr read: %p\n", (current_addr-1));
+  #endif
+
+  retval[core_id()] = errors;
 
   return;
 }
