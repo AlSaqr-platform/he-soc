a159bef47440cd643cc645dd505ccc737fe5950e
Luca Valente <luca.valente2@unibo.it>
2023-11-10 13:54:47 +0100

    Beautify hello world

--- software/hello_culsans/hello_culsans.c
+++ software/hello_culsans/hello_culsans.c
@@ -4,19 +4,26 @@
 #include "utils.h"
 #include "encoding.h"
 
+//#define FPGA_EMULATION
 __attribute__ ((section(".heapl2ram"))) int core_done;
 
-//#define FPGA_EMULATION
+static inline void cva6_barrier(int cid, int nc) {
+
+  while(core_done != (cid-1)){
+    __asm__ volatile("nop;");
+  }
+  core_done++;
+  if(cid==nc-1)
+    core_done = -1;
+
+}
+
 int thread_entry(int cid, int nc){
 
-  if(cid==0) {
+  int core_id =  read_csr(mhartid);
+  if(core_id==0)
     core_done = -1;
-    while(core_done!=0){
-      __asm__ volatile("nop;");
-    }
-  }
-  if(cid==1)
-    core_done = 0;
+  cva6_barrier(core_id,nc);
 
   #ifdef FPGA_EMULATION
   int baud_rate = 115200;
@@ -29,17 +36,13 @@ int thread_entry(int cid, int nc){
   #endif
   uart_set_cfg(0,(test_freq/baud_rate)>>4);
 
-  while(core_done<cid){
-    __asm__ volatile("nop;");
+  for(int i = 0; i<nc; i++) {
+    if(i==core_id) {
+      printf("Hello Culsans! I'm Core %d!\r\n", core_id);
+      uart_wait_tx_done();
+    }
+    cva6_barrier(core_id,nc);
   }
 
-  printf("Hello Culsans! I'm Core %d!\r\n", read_csr(mhartid));
-  uart_wait_tx_done();
-
-  core_done++;
-
-  while(core_done<2){
-    __asm__ volatile("nop;");
-  }
   return 0;
 }
