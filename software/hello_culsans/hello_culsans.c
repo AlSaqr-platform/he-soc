eb39cdd8f80282310a2096a773001017f15b912c
Luca <luca.valente@unibo.it>
2023-11-14 15:08:40 +0100

    Replace ugly barrier with Peterson lock.

--- software/hello_culsans/hello_culsans.c
+++ software/hello_culsans/hello_culsans.c
@@ -4,30 +4,36 @@
 #include "utils.h"
 #include "encoding.h"
 
-//#define FPGA_EMULATION
-__attribute__ ((section(".heapl2ram"))) int core_done;
+#define FPGA_EMULATION
 
-static inline void cva6_barrier(int cid, int nc) {
+__attribute__ ((section(".heapl2ram"))) volatile int flag[2] = { 0, 0};
 
-  while(core_done != (cid-1)){
+__attribute__ ((section(".heapl2ram"))) volatile int victim = -1;
+
+__attribute__ ((section(".heapl2ram"))) volatile int core_1 = -1;
+
+static __attribute__ ((noinline)) void lock(int cid) {
+  flag[cid] = 1;
+  victim = cid;
+  int other_cid = 1-cid;
+  while( flag[other_cid] && (victim==cid) ) {
     __asm__ volatile("nop;");
   }
-  core_done++;
-  if(cid==nc-1)
-    core_done = -1;
+}
 
+static __attribute__ ((noinline)) void unlock(int cid) {
+  flag[cid] = 0;
 }
 
 int thread_entry(int cid, int nc){
 
   int core_id =  read_csr(mhartid);
-  if(core_id==0)
-    core_done = -1;
-  cva6_barrier(core_id,nc);
+
+  lock(cid);
 
   #ifdef FPGA_EMULATION
   int baud_rate = 115200;
-  int test_freq = 50000000;
+  int test_freq = 40000000;
   alsaqr_periph_fpga_padframe_periphs_cva6_uart_00_mux_set(1);
   #else
   set_flls();
@@ -36,13 +42,10 @@ int thread_entry(int cid, int nc){
   #endif
   uart_set_cfg(0,(test_freq/baud_rate)>>4);
 
-  for(int i = 0; i<nc; i++) {
-    if(i==core_id) {
-      printf("Hello Culsans! I'm Core %d!\r\n", core_id);
-      uart_wait_tx_done();
-    }
-    cva6_barrier(core_id,nc);
-  }
+  printf("Hello Culsans! I'm Core %d!\r\n", core_id);
+  uart_wait_tx_done();
+
+  unlock(cid);
 
   return 0;
 }
