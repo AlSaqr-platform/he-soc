44c600f0c25d8ec361cf773863a8265eb236e731
Maicol Ciani <mciani@lagrev5.ee.ethz.ch>
2023-11-21 12:50:29 +0100

    Bumping axi riscv atomics: fix llc performance bug.

--- hardware/host/cva6_subsystem.sv
+++ hardware/host/cva6_subsystem.sv
@@ -387,7 +387,8 @@ module cva6_subsystem
     .AXI_DATA_WIDTH     ( AXI_DATA_WIDTH           ),
     .AXI_ID_WIDTH       ( ariane_soc::IdWidthSlave ),
     .AXI_USER_WIDTH     ( AXI_USER_WIDTH           ),
-    .AXI_MAX_WRITE_TXNS ( 1                        ),
+    .AXI_MAX_READ_TXNS  ( 8                        ),
+    .AXI_MAX_WRITE_TXNS ( 8                        ),
     .RISCV_WORD_WIDTH   ( 64                       )
   ) i_axi_riscv_atomicsl2 (
     .clk_i,
@@ -452,7 +453,8 @@ module cva6_subsystem
     .AXI_DATA_WIDTH     ( AXI_DATA_WIDTH           ),
     .AXI_ID_WIDTH       ( ariane_soc::IdWidthSlave ),
     .AXI_USER_WIDTH     ( AXI_USER_WIDTH           ),
-    .AXI_MAX_WRITE_TXNS ( 1                        ),
+    .AXI_MAX_READ_TXNS  ( 8                        ),
+    .AXI_MAX_WRITE_TXNS ( 8                        ),
     .RISCV_WORD_WIDTH   ( 64                       )
   ) i_axi_riscv_atomicsl3 (
     .clk_i,
