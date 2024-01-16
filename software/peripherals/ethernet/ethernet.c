cc6175b4d35a896fe103af7a859b2104f3504fd5
Marco Fontana <ciabadiale@gmail.com>
2024-01-16 16:18:05 +0100

    adding test for the ethernet when using simple padframe

--- software/peripherals/ethernet/ethernet.c
+++ software/peripherals/ethernet/ethernet.c
@@ -4,11 +4,12 @@
 #include <time.h>
 #include "utils.h"
 
-   
 #define DRIVER_NAME     "alsaqr-eth"
 #define BUFFER_SIZE      8
 #define WORD_SIZE        8
 #define N_REPS           2
+
+#define SIMPLE_PAD
 	
 #define ETH_BASE 			  		 				 0x30000000
 // #define GPIO_PADDIR_0_31_OFFSET      0x0
@@ -25,98 +26,57 @@
 #define TXBUFF_OFFSET                0x1000
 // RX buffer
 #define RXBUFF_OFFSET                0x4000
-
-
-
   
 int main() {
-	   
 
-		
-	// for (int v = 0; v < N_REPS; v++) {
-	// 	#ifdef FPGA_EMULATION
-	// 		return 0;
-	// 	#else
-	// 		#ifdef SIMPLE_PAD
-	// 		return 0;
-	// 	#else
-	// 		switch(v){
-	// 		case 0:
-	// 		alsaqr_periph_padframe_periphs_a_00_mux_set( 2 );  
-
-			// alsaqr_periph_padframe_periphs_b_23_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_24_mux_set( 1 ); 
-			// alsaqr_periph_padframe_periphs_b_25_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_26_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_27_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_28_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_29_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_30_mux_set( 2 );
-			// alsaqr_periph_padframe_periphs_b_31_mux_set( 2 );
-			// alsaqr_periph_padframe_periphs_b_32_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_33_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_34_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_35_mux_set( 1 );
-			// alsaqr_periph_padframe_periphs_b_36_mux_set( 2 );
-			// alsaqr_periph_padframe_periphs_b_37_mux_set( 2 );
-
-
-   //    alsaqr_periph_padframe_periphs_a_15_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_16_mux_set( 0);
-			// alsaqr_periph_padframe_periphs_a_17_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_18_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_19_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_20_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_21_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_22_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_23_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_24_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_25_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_26_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_27_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_28_mux_set( 0 );
-			// alsaqr_periph_padframe_periphs_a_29_mux_set( 0 );
-			// break;
-			// case 1:
-			// alsaqr_periph_padframe_periphs_a_00_mux_set( 2 );  
-
-			alsaqr_periph_padframe_periphs_b_23_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_24_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_25_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_26_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_27_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_28_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_29_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_30_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_31_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_32_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_33_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_34_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_35_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_36_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_b_37_mux_set( 0 );
-			alsaqr_periph_padframe_periphs_a_15_mux_set( 1 );
-			alsaqr_periph_padframe_periphs_a_16_mux_set( 1 );
-			alsaqr_periph_padframe_periphs_a_17_mux_set( 1 );
-			alsaqr_periph_padframe_periphs_a_18_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_19_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_20_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_21_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_22_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_23_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_24_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_25_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_26_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_27_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_28_mux_set( 2 );
-			alsaqr_periph_padframe_periphs_a_29_mux_set( 1 );
-	// 		break;
-	// 		}
-	// 		#endif    
-	// 	 #endif
-	// }
-
-    
+	#ifdef SIMPLE_PAD
+		alsaqr_periph_padframe_periphs_a_00_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_01_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_02_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_03_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_04_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_05_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_06_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_07_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_08_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_09_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_10_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_11_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_12_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_13_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_14_mux_set( 1 );
+	#else
+		alsaqr_periph_padframe_periphs_b_23_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_24_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_25_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_26_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_27_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_28_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_29_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_30_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_31_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_32_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_33_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_34_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_35_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_36_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_b_37_mux_set( 0 );
+		alsaqr_periph_padframe_periphs_a_15_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_16_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_17_mux_set( 1 );
+		alsaqr_periph_padframe_periphs_a_18_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_19_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_20_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_21_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_22_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_23_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_24_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_25_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_26_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_27_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_28_mux_set( 2 );
+		alsaqr_periph_padframe_periphs_a_29_mux_set( 1 );
+	#endif
 
     // Write data to respective addresses
     *(volatile uint64_t *)0x30001000 = 0x1032207098001032;  // 1 --> 230100890702 2301, mac dest + start of mac source
@@ -128,10 +88,10 @@ int main() {
     *(volatile uint64_t *)0x30001030 = 0x5756555453525150;
     *(volatile uint64_t *)0x30001038 = 0x6766656463626160;
 
-    pulp_write32( ETH_BASE + TPLR_OFFSET , 0x00000040 );          
-    pulp_write32( ETH_BASE + MACLO_OFFSET , 0x00890702 );
-    pulp_write32( ETH_BASE + MACHI_OFFSET , 0x00802301 );
-    pulp_write32( ETH_BASE + RFCS_OFFSET ,   0x00000008 );	
+    pulp_write32( ETH_BASE + TPLR_OFFSET,  0x00000040 );          
+    pulp_write32( ETH_BASE + MACLO_OFFSET, 0x00890702 );
+    pulp_write32( ETH_BASE + MACHI_OFFSET, 0x00802301 );
+    pulp_write32( ETH_BASE + RFCS_OFFSET,  0x00000008 );	
 
     while(1);
 
