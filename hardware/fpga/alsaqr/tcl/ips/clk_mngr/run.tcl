2975904c97ab498a795780fa39292801966218be
Marco Fontana <ciabadiale@gmail.com>
2024-01-29 15:19:32 +0100

    clk ip now generates 200MHz and 125MHz for the ethernet

--- hardware/fpga/alsaqr/tcl/ips/clk_mngr/run.tcl
+++ hardware/fpga/alsaqr/tcl/ips/clk_mngr/run.tcl
@@ -19,18 +19,44 @@ create_project $ipName . -part $partNumber
 set_property board_part $boardName [current_project]
 
 create_ip -name clk_wiz -vendor xilinx.com -library ip -module_name $ipName
-set_property -dict [list CONFIG.PRIM_IN_FREQ {250.000} \
-                        CONFIG.CLKOUT1_REQUESTED_OUT_FREQ "$CLK_FREQ_MHZ" \
-                        CONFIG.RESET_TYPE {ACTIVE_LOW} \
-                        CONFIG.CLKIN1_JITTER_PS {40.0} \
-                        CONFIG.MMCM_DIVCLK_DIVIDE {25} \
-                        CONFIG.MMCM_CLKFBOUT_MULT_F {105.750} \
-                        CONFIG.MMCM_CLKIN1_PERIOD {4.000} \
-                        CONFIG.MMCM_CLKIN2_PERIOD {10.0} \
-                        CONFIG.MMCM_CLKOUT0_DIVIDE_F {88.125} \
-                        CONFIG.RESET_PORT {resetn} \
-                        CONFIG.CLKOUT1_JITTER {350.612} \
-                        CONFIG.CLKOUT1_PHASE_ERROR {426.712} ] [get_ips $ipName]
+
+if {$::env(ETH2FMC_NO_PAD)=="1"} {
+    set_property -dict [list CONFIG.PRIM_IN_FREQ {250} \
+                            CONFIG.CLKOUT2_USED {true} \
+                            CONFIG.CLKOUT3_USED {true} \
+                            CONFIG.CLKOUT1_REQUESTED_OUT_FREQ "$CLK_FREQ_MHZ" \
+                            CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {125} \
+                            CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {200} \
+                            CONFIG.CLKIN1_JITTER_PS {40.0} \
+                            CONFIG.MMCM_DIVCLK_DIVIDE {1} \
+                            CONFIG.MMCM_CLKFBOUT_MULT_F {4.000} \
+                            CONFIG.MMCM_CLKIN1_PERIOD {4.000} \
+                            CONFIG.MMCM_CLKIN2_PERIOD {10.0} \
+                            CONFIG.MMCM_CLKOUT0_DIVIDE_F {25.000} \
+                            CONFIG.MMCM_CLKOUT1_DIVIDE {8} \
+                            CONFIG.MMCM_CLKOUT2_DIVIDE {5} \
+                            CONFIG.NUM_OUT_CLKS {3} \
+                            CONFIG.CLKOUT1_JITTER {128.769} \
+                            CONFIG.CLKOUT1_PHASE_ERROR {85.928} \
+                            CONFIG.CLKOUT2_JITTER {102.531} \
+                            CONFIG.CLKOUT2_PHASE_ERROR {85.928} \
+                            CONFIG.CLKOUT3_JITTER {93.521} \
+                            CONFIG.CLKOUT3_PHASE_ERROR {85.928} \
+                            ] [get_ips $ipName]
+else {
+    set_property -dict [list CONFIG.PRIM_IN_FREQ {250.000} \
+                            CONFIG.CLKOUT1_REQUESTED_OUT_FREQ "$CLK_FREQ_MHZ" \
+                            CONFIG.RESET_TYPE {ACTIVE_LOW} \
+                            CONFIG.CLKIN1_JITTER_PS {40.0} \
+                            CONFIG.MMCM_DIVCLK_DIVIDE {25} \
+                            CONFIG.MMCM_CLKFBOUT_MULT_F {105.750} \
+                            CONFIG.MMCM_CLKIN1_PERIOD {4.000} \
+                            CONFIG.MMCM_CLKIN2_PERIOD {10.0} \
+                            CONFIG.MMCM_CLKOUT0_DIVIDE_F {88.125} \
+                            CONFIG.RESET_PORT {resetn} \
+                            CONFIG.CLKOUT1_JITTER {350.612} \
+                            CONFIG.CLKOUT1_PHASE_ERROR {426.712} ] [get_ips $ipName]
+}
 
 generate_target all [get_files  ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
 create_ip_run [get_files -of_objects [get_fileset sources_1] ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
