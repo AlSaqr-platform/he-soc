35129d49532c12ba2547053f9944012915fb9174
Riccardo Tedeschi <riccardo.tedeschi4@studio.unibo.it>
2024-01-31 12:21:05 +0100

    Rework the simulation flow to streamline it

--- software/litmus/work.mk
+++
@@ -1,9 +0,0 @@
-include $(HW_HOME)/Makefile
-
-%.log: ../binaries/%.elf
-	echo "Processing $*"
-	@ln -sf $(HW_HOME)/work work
-	@ln -sf $(HW_HOME)/work-dpi work-dpi
-	@ln -sf $(HW_HOME)/tb tb
-	$(SW_HOME)/elf_to_slm.py --binary=$< --vectors=hyperram0.slm >& /dev/null
-	$(MAKE) run $(ADDITIONAL_FLAGS) elf-bin=$< 1> $@ 2> /dev/null
