fcc9f05f640e4ca6dec5928d09b8fc0d95928724
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2024-02-07 18:39:58 +0100

    Add I$ warming before profiling

--- software/culsans_bare_perf/testlist/write_hit_shared_busy/write_hit_shared_busy.c
+++ software/culsans_bare_perf/testlist/write_hit_shared_busy/write_hit_shared_busy.c
@@ -17,15 +17,11 @@ void prepare()
 
 int write_hit_shared_busy(int cid, int nc)
 {
-  long begin, end;
-
   if (cid == 0) {
-    for (int i = 0; i < sizeof(data)/sizeof(data[0]); i++)
-      data[i];
-    begin = rdcycle();
-    unrolled_write();
-    end = rdcycle();
-    exit((end-begin)>>12);
+    // `data` should be cached and shared -> read it
+    read_loop(data, CACHE_WAYS * CACHE_ENTRIES);
+    profile(unrolled_write, 1);
+    exit(0);
   }
 
   // create some traffic
--- software/culsans_bare_perf/testlist/write_shared_remote/write_shared_remote.c
+++ software/culsans_bare_perf/testlist/write_shared_remote/write_shared_remote.c
@@ -6,16 +6,17 @@
 extern void exit(int);
 
 volatile cacheline_t data[CACHE_WAYS * CACHE_ENTRIES] __attribute__((section(".cache_share_region")));
+volatile cacheline_t more_data[CACHE_WAYS * CACHE_ENTRIES] __attribute__((section(".cache_share_region")));
 
 int write_shared_remote(int cid, int nc)
 {
   long begin, end;
 
   if (cid == 0) {
-    begin = rdcycle();
-    unrolled_write();
-    end = rdcycle();
-    exit((end-begin)>>12);
+    warm(unrolled_write, 2);
+    read_loop(more_data, CACHE_WAYS * CACHE_ENTRIES);
+    profile(unrolled_write, 1);
+    exit(0);
   }
 
   return 0;
