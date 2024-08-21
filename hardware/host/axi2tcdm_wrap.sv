b80ddd1709d1163f64bfae4491d3be5ad723aeaf
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2024-08-21 15:59:32 +0200

    Replace `axi2tcdm` with `axi_to_mem_split`

--- hardware/host/axi2tcdm_wrap.sv
+++ hardware/host/axi2tcdm_wrap.sv
@@ -34,9 +34,9 @@ module axi2tcdm_wrap
 );
 
   logic [NB_DMAS-1:0][31:0] s_tcdm_bus_wdata;
-  logic [NB_DMAS-1:0][31:0] s_tcdm_bus_add;
+  logic [NB_DMAS-1:0][63:0] s_tcdm_bus_add;
   logic [NB_DMAS-1:0]       s_tcdm_bus_req;
-  logic [NB_DMAS-1:0]       s_tcdm_bus_wen;
+  logic [NB_DMAS-1:0]       s_tcdm_bus_we;
   logic [NB_DMAS-1:0][3:0]  s_tcdm_bus_be;
   logic [NB_DMAS-1:0]       s_tcdm_bus_gnt;
   logic [NB_DMAS-1:0][31:0] s_tcdm_bus_r_rdata;
@@ -44,10 +44,10 @@ module axi2tcdm_wrap
 
   generate
     for (genvar i=0; i<NB_DMAS; i++) begin : TCDM_BIND
-      assign tcdm_master[i].add    = s_tcdm_bus_add[i];
+      assign tcdm_master[i].add    = s_tcdm_bus_add[i][31:0];
       assign tcdm_master[i].req    = s_tcdm_bus_req[i];
       assign tcdm_master[i].wdata  = s_tcdm_bus_wdata[i];
-      assign tcdm_master[i].wen    = s_tcdm_bus_wen[i];
+      assign tcdm_master[i].wen    = !s_tcdm_bus_we[i];
       assign tcdm_master[i].be     = s_tcdm_bus_be[i];
 
 
@@ -57,77 +57,30 @@ module axi2tcdm_wrap
     end
   endgenerate
 
-  axi2tcdm #(
-    .AXI_ADDR_WIDTH       ( AXI_ADDR_WIDTH        ),
-    .AXI_DATA_WIDTH       ( AXI_DATA_WIDTH        ),
-    .AXI_USER_WIDTH       ( AXI_USER_WIDTH        ),
-    .AXI_ID_WIDTH         ( AXI_ID_WIDTH          )
+  axi_to_mem_split_intf #(
+    .AXI_ID_WIDTH   (AXI_ID_WIDTH  ),
+    .AXI_ADDR_WIDTH (AXI_ADDR_WIDTH),
+    .AXI_DATA_WIDTH (AXI_DATA_WIDTH),
+    .AXI_USER_WIDTH (AXI_USER_WIDTH),
+    .MEM_DATA_WIDTH (32            ),
+    .BUF_DEPTH      (4             ),
+    .HIDE_STRB      (1'b0          ),
+    .OUT_FIFO_DEPTH (32'd1         )
   ) axi2mem_i (
-    .clk_i                 ( clk_i                ),
-    .rst_ni                ( rst_ni               ),
-    //.slave                 ( axi_slave            ),
-
-    .tcdm_master_req_o                 ( s_tcdm_bus_req       ),
-    .tcdm_master_add_o                ( s_tcdm_bus_add       ),
-    .tcdm_master_type_o                  ( s_tcdm_bus_wen       ),
-    .tcdm_master_data_o                ( s_tcdm_bus_wdata     ),
-    .tcdm_master_be_o                  ( s_tcdm_bus_be        ),
-    .tcdm_master_gnt_i     ( s_tcdm_bus_gnt       ),
-
-    .tcdm_master_r_valid_i ( s_tcdm_bus_r_valid   ),
-    .tcdm_master_r_data_i                ( s_tcdm_bus_r_rdata   ),
-
-    .busy_o                ( busy_o               ),
-    .test_en_i             ( test_en_i            ),
-
-    .axi_slave_aw_valid_i  ( axi_slave.aw_valid   ),
-    .axi_slave_aw_addr_i   ( axi_slave.aw_addr    ),
-    .axi_slave_aw_prot_i   ( axi_slave.aw_prot    ),
-    .axi_slave_aw_region_i ( axi_slave.aw_region  ),
-    .axi_slave_aw_len_i    ( axi_slave.aw_len     ),
-    .axi_slave_aw_size_i   ( axi_slave.aw_size    ),
-    .axi_slave_aw_burst_i  ( axi_slave.aw_burst   ),
-    .axi_slave_aw_lock_i   ( axi_slave.aw_lock    ),
-    .axi_slave_aw_cache_i  ( axi_slave.aw_cache   ),
-    .axi_slave_aw_qos_i    ( axi_slave.aw_qos     ),
-    .axi_slave_aw_id_i     ( axi_slave.aw_id      ),
-    .axi_slave_aw_user_i   ( axi_slave.aw_user    ),
-    .axi_slave_aw_ready_o  ( axi_slave.aw_ready   ),
-
-    .axi_slave_ar_valid_i  ( axi_slave.ar_valid   ),
-    .axi_slave_ar_addr_i   ( axi_slave.ar_addr    ),
-    .axi_slave_ar_prot_i   ( axi_slave.ar_prot    ),
-    .axi_slave_ar_region_i ( axi_slave.ar_region  ),
-    .axi_slave_ar_len_i    ( axi_slave.ar_len     ),
-    .axi_slave_ar_size_i   ( axi_slave.ar_size    ),
-    .axi_slave_ar_burst_i  ( axi_slave.ar_burst   ),
-    .axi_slave_ar_lock_i   ( axi_slave.ar_lock    ),
-    .axi_slave_ar_cache_i  ( axi_slave.ar_cache   ),
-    .axi_slave_ar_qos_i    ( axi_slave.ar_qos     ),
-    .axi_slave_ar_id_i     ( axi_slave.ar_id      ),
-    .axi_slave_ar_user_i   ( axi_slave.ar_user    ),
-    .axi_slave_ar_ready_o  ( axi_slave.ar_ready   ),
-
-    .axi_slave_w_valid_i   ( axi_slave.w_valid    ),
-    .axi_slave_w_data_i    ( axi_slave.w_data     ),
-    .axi_slave_w_strb_i    ( axi_slave.w_strb     ),
-    .axi_slave_w_user_i    ( axi_slave.w_user     ),
-    .axi_slave_w_last_i    ( axi_slave.w_last     ),
-    .axi_slave_w_ready_o   ( axi_slave.w_ready    ),
-
-    .axi_slave_r_valid_o   ( axi_slave.r_valid    ),
-    .axi_slave_r_data_o    ( axi_slave.r_data     ),
-    .axi_slave_r_resp_o    ( axi_slave.r_resp     ),
-    .axi_slave_r_last_o    ( axi_slave.r_last     ),
-    .axi_slave_r_id_o      ( axi_slave.r_id       ),
-    .axi_slave_r_user_o    ( axi_slave.r_user     ),
-    .axi_slave_r_ready_i   ( axi_slave.r_ready    ),
-
-    .axi_slave_b_valid_o   ( axi_slave.b_valid    ),
-    .axi_slave_b_resp_o    ( axi_slave.b_resp     ),
-    .axi_slave_b_id_o      ( axi_slave.b_id       ),
-    .axi_slave_b_user_o    ( axi_slave.b_user     ),
-    .axi_slave_b_ready_i   ( axi_slave.b_ready    )
+    .clk_i        (clk_i),
+    .rst_ni       (rst_ni),
+    .test_i       (test_en_i),
+    .busy_o       (busy_o),
+    .axi_bus      (axi_slave),
+    .mem_req_o    (s_tcdm_bus_req),
+    .mem_gnt_i    (s_tcdm_bus_gnt),
+    .mem_addr_o   (s_tcdm_bus_add),
+    .mem_wdata_o  (s_tcdm_bus_wdata),
+    .mem_strb_o   (s_tcdm_bus_be),
+    .mem_atop_o   (),
+    .mem_we_o     (s_tcdm_bus_we),
+    .mem_rvalid_i (s_tcdm_bus_r_valid),
+    .mem_rdata_i  (s_tcdm_bus_r_rdata)
   );
 
 endmodule
