80980fdb52d407a8bbfca3acd0b3fa6df32ce326
Maicol Ciani <maicol.ciani@unibo.it>
2024-09-30 23:23:19 +0200

    Code clean up

--- hardware/deps/monitor_counters_t/rtl/monitor_counters_t_event.sv
+++ hardware/deps/monitor_counters_t/rtl/monitor_counters_t_event.sv
@@ -1,9 +1,12 @@
-// Copyright lowRISC contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-//
-// Description: UART core module
-//
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the “License”); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
 
 module monitor_counters_t_event (
   input                  clk_i,
