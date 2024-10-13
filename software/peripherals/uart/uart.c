894216eec27702ca60647efa5e6fce61aac26078
Chaoqun Liang <chaoqun.liang@unibo.it>
2024-10-13 19:57:10 +0200

    uart aplic

--- software/peripherals/uart/uart.c
+++ software/peripherals/uart/uart.c
@@ -28,11 +28,7 @@
 
 #define BUFFER_SIZE 16
 #define UART_BAUDRATE 115200
-
-#define PLIC_BASE 0x0C000000
-#define PLIC_CHECK PLIC_BASE + 0x201004
-//enable bits for sources 0-31
-#define PLIC_EN_BITS  PLIC_BASE + 0x2080
+#define CSR_MTOPEI 0x35C
 
 /*******************************************************************************
 **                             IMPORTANT                                      **
@@ -101,6 +97,8 @@ int main()
 
   uint32_t tx_uart_plic_id ;
   uint32_t rx_uart_plic_id ; 
+  uint32_t rx_uart_imsic_id;
+  uint32_t tx_uart_imsic_id;
 
   #ifdef FPGA_EMULATION
   int baud_rate = 115200;
@@ -114,6 +112,9 @@ int main()
 
   printf("uart start\n");
   uart_wait_tx_done();
+
+  aplic_init();
+  imsic_init();
 
   tx_buffer[0]= 'S';
   tx_buffer[1]= 't';
@@ -183,45 +184,30 @@ int main()
 
       rx_uart_plic_id = ARCHI_UDMA_UART_ID(u)*4 +16 ; //24
       tx_uart_plic_id = ARCHI_UDMA_UART_ID(u)*4 +16 +1; //25
-
+      tx_uart_imsic_id = 1;
+      rx_uart_imsic_id = 2;
+      
+      config_irq_aplic(tx_uart_plic_id, tx_uart_imsic_id, 0);
+      config_irq_aplic(rx_uart_plic_id, rx_uart_imsic_id, 0);
+     
       //printf("[%d, %d] Start test uart %d\n",  get_cluster_id(), get_core_id(), u);
       udma_uart_open(u,test_freq,UART_BAUDRATE);
 
       for (int i = 0; i < BUFFER_SIZE; ++i)
       {
         uart_write_nb(u,tx_buffer + i,1); //--- non blocking write
-        
-        //set tx interrupt
-        pulp_write32(PLIC_BASE+tx_uart_plic_id*4, 1); // set rx interrupt priority to 1
 
-        //enable interrupt
-        pulp_write32(PLIC_EN_BITS+(((int)(tx_uart_plic_id/32))*4), 1<<(tx_uart_plic_id%32)); //enable interrupt
-
-        //  wfi until reading the rx id from the PLIC
-        while(pulp_read32(PLIC_CHECK)!=tx_uart_plic_id) {
-          asm volatile ("wfi");
-        }
-
-        //Set completed Interrupt
-        pulp_write32(PLIC_CHECK,tx_uart_plic_id);
+        asm volatile ("wfi");
+        imsic_intp_arrive(tx_uart_imsic_id);
+        CSRW(CSR_MTOPEI, 0);
 
         //printf("TX Interrupt received\n\r");
 
         udma_uart_read(u,rx_buffer + i,1);     //--- blocking read
-
-        //set rx interrupt
-        pulp_write32(PLIC_BASE+rx_uart_plic_id*4, 1); // set rx interrupt priority to 1
-
-        //enable interrupt
-        pulp_write32(PLIC_EN_BITS+(((int)(rx_uart_plic_id/32))*4), 1<<(rx_uart_plic_id%32)); //enable interrupt
-
-        //  wfi until reading the rx id from the PLIC
-        while(pulp_read32(PLIC_CHECK)!=rx_uart_plic_id) {
-          asm volatile ("wfi");
-        }
-
-        //Set completed Interrupt
-        pulp_write32(PLIC_CHECK,rx_uart_plic_id);
+        
+        asm volatile ("wfi");
+        imsic_intp_arrive(rx_uart_imsic_id);
+        CSRW(CSR_MTOPEI, 0);
 
         //printf("RX Interrupt received\n\r");
         
@@ -232,11 +218,14 @@ int main()
           printf("UART_%0d.%0d FAIL: tx %c, rx %c\n\r", u, v, tx_buffer[i],rx_buffer[i]);
           error++;
         }
+
+        aplic_reset(tx_uart_imsic_id);
+        aplic_reset(rx_uart_imsic_id);
       }
       udma_uart_close(u);
     }
   }
-
+  
   if (error==0)
     printf("Test PASSED\n\r");
   else
