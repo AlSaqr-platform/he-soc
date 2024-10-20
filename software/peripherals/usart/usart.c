c5b0a099b7888e4cd8ec1309063c2fe0621d0b94
Chaoqun Liang <chaoqun.liang@unibo.it>
2024-10-20 13:33:05 +0200

    0.1 stuck

--- software/peripherals/usart/usart.c
+++ software/peripherals/usart/usart.c
@@ -23,6 +23,7 @@
 #include <stdint.h>
 #include "utils.h"
 #include "udma.h"
+#include "aplic.h"
 
 #include "udma_uart_v1.h"
 
@@ -77,12 +78,15 @@ int main()
   int *tx_buffer= (int*) 0x1C001000;
 
   //int rx_buffer[BUFFER_SIZE];
-  int *rx_buffer= (int*) 0x1C002000;
+  int *rx_buffer= (int*) 0x1C002000;   // L2
 
   uint32_t tx_usart_plic_id ;
   uint32_t rx_usart_plic_id ; 
   uint32_t tx_usart_imsic_id ;
   uint32_t rx_usart_imsic_id ;
+
+  aplic_init();
+  imsic_init();
 
   #ifdef FPGA_EMULATION
   int baud_rate = 115200;
@@ -96,9 +100,6 @@ int main()
 
   printf("USART start...\n\r");
   uart_wait_tx_done();
-
-  aplic_init();
-  imsic_init();
 
   tx_buffer[0]= 'S';
   tx_buffer[1]= 't';
@@ -198,29 +199,35 @@ int main()
       tx_usart_plic_id = ARCHI_UDMA_USART_ID(u)*4 +16 +1;
       rx_usart_imsic_id = 1;
       tx_usart_imsic_id = 2;
-      
+      // try rx= 1
+      // move reset to up
+      // config?
+
       config_irq_aplic(tx_usart_plic_id, tx_usart_imsic_id, 0);
       config_irq_aplic(rx_usart_plic_id, rx_usart_imsic_id, 0);
+
       //printf("[%d, %d] Start test uart %d\n",  get_cluster_id(), get_core_id(), u);
       udma_usart_open(u,test_freq,UART_BAUDRATE);
 
       for (int i = 0; i < BUFFER_SIZE; ++i)
       {
         usart_write_nb(u,tx_buffer + i,1); //--- non blocking write
-        
+        // it wont start if config separate 
+        //config_irq_aplic(tx_usart_plic_id, tx_usart_imsic_id, 0);
         asm volatile ("wfi");
         imsic_intp_arrive(tx_usart_imsic_id);
         CSRW(CSR_MTOPEI, 0);
 
-        //printf("TX Interrupt received\n\r");
+        printf("TX Interrupt received\n\r");
 
         udma_usart_read(u,rx_buffer + i,1);     //--- blocking read
-
+        
+        //config_irq_aplic(rx_usart_plic_id, rx_usart_imsic_id, 0);
         asm volatile ("wfi");
         imsic_intp_arrive(rx_usart_imsic_id);
         CSRW(CSR_MTOPEI, 0);
 
-        //printf("RX Interrupt received\n\r");
+        printf("RX Interrupt received\n\r");
         
         if (tx_buffer[i] == rx_buffer[i])
         {
@@ -229,6 +236,8 @@ int main()
           printf("USART_%0d.%0d[control flow OFF] FAIL: tx %c, rx %c\n\r", u, v, tx_buffer[i],rx_buffer[i]);
           error++;
         }
+        aplic_reset(tx_usart_imsic_id);
+        aplic_reset(rx_usart_imsic_id);
       }
 
       printf("USART_%0d.%0d[control flow ON] test started...\n\r", u, v);
@@ -266,8 +275,6 @@ int main()
           printf("USART_%0d.%0d[control flow ON] FAIL: tx %c, rx %c\n\r", u, v, tx_buffer[i],rx_buffer[i]);
           error++;
         }
-      aplic_reset(tx_usart_imsic_id);
-      aplic_reset(rx_usart_imsic_id);
       }
       udma_usart_close(u);
     }
