f9e4e15b2614d95d1e5eebf5b0aeb0fb7c9ae29a
Chaoqun Liang <chaoqun.liang@unibo.it>
2024-10-24 12:09:13 +0200

    Finalize gpio test. WIP sdio.

--- software/gpios_all/gpios_all.c
+++ software/gpios_all/gpios_all.c
@@ -52,6 +52,28 @@
 
 #define VERBOSE 1
 
+uint32_t irq_configure ( uint32_t number, uint32_t irq_en){
+  uint32_t address;
+  uint32_t gpio_inten;
+  if(number < 32){
+    address = ARCHI_GPIO_ADDR + GPIO_INTEN_OFFSET;
+    gpio_inten = pulp_read32(address);
+    //--- enable GPIO
+    //printf("GPIO_INTEN RD: %x\n",gpio_inten);
+    gpio_inten |= (1 << number);
+    //printf("GPIO_INTEN WR: %x\n",gpio_inten); 
+    pulp_write32(address, gpio_inten); 
+  } else {
+    address = ARCHI_GPIO_ADDR + GPIO_INTEN_32_63_OFFSET;
+    gpio_inten = pulp_read32(address);
+    //--- enable GPIO
+    //printf("GPIO_INTEN RD: %x\n",gpio_inten);
+    gpioen |= (1 << (number-32));
+    //printf("GPIO_INTEN WR: %x\n",gpio_inten);  
+    pulp_write32(address, gpioen);
+  }
+}
+
 uint32_t configure_gpio(uint32_t number, uint32_t direction){
   uint32_t address;
   uint32_t dir;
