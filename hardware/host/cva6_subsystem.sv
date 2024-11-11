e2acb93477edb69268126018dcae4fede8dd3296
Maicol Ciani <maicol.ciani@unibo.it>
2024-11-11 08:43:07 +0100

    Fix directionlity of AXI PMU's interface

--- hardware/host/cva6_subsystem.sv
+++ hardware/host/cva6_subsystem.sv
@@ -78,7 +78,7 @@ module cva6_subsystem
   AXI_BUS.Master          apb_axi_master,
   AXI_BUS.Master          hyper_axi_master,
   AXI_BUS.Master          cluster_axi_master,
-  AXI_BUS.Master          pmu_axi_master,
+  AXI_BUS.Slave           pmu_axi_master,
   AXI_BUS.Slave           cluster_axi_slave,
   AXI_BUS.Slave           udma_rx_l3_axi_slave,
   AXI_BUS.Slave           udma_tx_l3_axi_slave,
