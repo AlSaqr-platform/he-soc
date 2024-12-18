c262ae52c49f646b3f00e225da5f8873d612517a
Abdur Rahman <rao.arrn@gmail.com>
2024-12-18 17:25:55 -0500

    Toolchain path updated

--- setup.sh
+++ setup.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 echo "exporting RISCV"
-
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin/riscv32-unknown-elf:$PATH
-
-export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH
-
-export RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0
-
+export PATH=/opt/riscv64/bin:$PATH
+export RISCV=/opt/riscv64
 export SW_HOME=$(pwd)/software
-
 export HW_HOME=$(pwd)/hardware
-
 echo "exporting QUESTASIM PATH"
-
-export QUESTASIM_HOME=/usr/pack/questa-2022.3-bt/questasim/
-
-export QUESTA=questa-2022.3-bt
-
+export QUESTASIM_HOME=/tools/questasim/
 echo "exporting RISCV 32 bit with zfinx"
-
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
-
+export PATH=/opt/riscv/bin:$PATH
 echo "cloning submodules"
-
-ulimit -n 2048
-
 git submodule update --init --recursive
