667b9686dffb2bcb0e2716dc40dbc0130ff65d6b
Maicol Ciani <maicol.ciani@unibo.it>
2025-01-12 20:46:27 +0100

    Include setup.sh for uw

--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 echo "exporting RISCV"
-export PATH=/opt/riscv64/bin:$PATH
-export RISCV=/opt/riscv64
+
+export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin/riscv32-unknown-elf:$PATH
+
+export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH
+
+export RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0
+
 export SW_HOME=$(pwd)/software
+
 export HW_HOME=$(pwd)/hardware
+
 echo "exporting QUESTASIM PATH"
-export QUESTASIM_HOME=/tools/questasim/
+
+export QUESTASIM_HOME=/usr/pack/questa-2022.3-bt/questasim/
+
+export QUESTA=questa-2022.3-bt
+
 echo "exporting RISCV 32 bit with zfinx"
-export PATH=/opt/riscv/bin:$PATH
+
+export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
+
 echo "cloning submodules"
+
+ulimit -n 2048
+
 git submodule update --init --recursive
---
+++ uw_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "exporting RISCV"
+export PATH=/opt/riscv64/bin:$PATH
+export RISCV=/opt/riscv64
+export SW_HOME=$(pwd)/software
+export HW_HOME=$(pwd)/hardware
+echo "exporting QUESTASIM PATH"
+export QUESTASIM_HOME=/tools/questasim/
+echo "exporting RISCV 32 bit with zfinx"
+export PATH=/opt/riscv/bin:$PATH
+echo "cloning submodules"
+git submodule update --init --recursive
