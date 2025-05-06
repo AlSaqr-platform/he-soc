7e408fecc71d993c4ee78a85880760f25eae346d
Maicol Ciani <maicol.ciani@unibo.it>
2025-05-06 10:07:36 +0200

    Update regression tests list (both netlsit and RTL).

--- hardware/tb/ariane_tb.sv
+++ hardware/tb/ariane_tb.sv
@@ -48,13 +48,9 @@ import "DPI-C" context function byte read_section(input longint address, inout b
 module ariane_tb;
 
   static uvm_cmdline_processor uvcl = uvm_cmdline_processor::get_inst();
-  `ifdef TARGET_POST_SYNTH_SIM_TOP
-  localparam int unsigned REFClockPeriod       = 1us; // jtag clock: 1MHz
-  `else
-  localparam int unsigned REFClockPeriod       = 25ns; // jtag clock: 40MzH
-  `endif
 
-  localparam int unsigned REFClockPeriodOt     = 25ns; // jtag clock: 40MHz
+  localparam int unsigned REFClockPeriod       = 1us;
+  localparam int unsigned REFClockPeriodOt     = 25ns;
 
   `ifdef ETH2FMC_NO_PADFRAME
     localparam int unsigned REFClockPeriod200     = 5ns; // eth200 clock: 200MHz
