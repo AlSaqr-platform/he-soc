a7965987627f808920469fbbabb72adf68001817
yvantor <yvan.tortorella@gmail.com>
2025-10-15 16:43:47 +0200

    Configure setup for Chips-IT servers.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "exporting RISCV"
 
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin/riscv32-unknown-elf:$PATH
+# export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin/riscv32-unknown-elf:$PATH
 
-export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH
+# export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH
 
-export RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0
+export RISCV=/opt/riscv/riscv64-15.1.0/
 
 export SW_HOME=$(pwd)/software
 
@@ -13,13 +13,13 @@ export HW_HOME=$(pwd)/hardware
 
 echo "exporting QUESTASIM PATH"
 
-export QUESTASIM_HOME=/usr/pack/questa-2022.3-bt/questasim/
+export QUESTASIM_HOME=/tools/siemens/questa_2022.3/questasim
 
-export QUESTA=questa-2022.3-bt
+export QUESTA=
 
 echo "exporting RISCV 32 bit with zfinx"
 
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
+# export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
 
 echo "cloning submodules"
 
