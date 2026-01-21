b1722c7f75cc2153c9b0a789244efa7bc1899793
Mattia Sinigaglia <mattia.sinigaglia5@unibo.it>
2026-01-21 09:54:51 +0100

    sw: upate fp32 PULP matmul - data in L1

--- software/par_matmul_fp32/stimuli/stimuli.c
+++ software/par_matmul_fp32/stimuli/stimuli.c
@@ -9,9 +9,11 @@
 #include "data.h"
 // #define FPGA_EMULATION
 
+#define GPIO2_PAD_A2_ADDR 0x1A10500C
+
 #ifdef FPGA_EMULATION
   int baud_rate = 115200;
-  int test_freq = 50000000;
+  int test_freq = 40000000;
 #else
   int baud_rate = 115200;
   int test_freq = 200000000;
@@ -27,13 +29,16 @@ void main_fn(int*);
 int retval = -1;
 
 int main () {
+  int cycle_start=0;
+  int cycle_end=0;
 
   synch_barrier();
-  
+
   //////////
   // TEST //
   //////////
 
+  //Executed by everyone
   main_fn(&retval);
 
   synch_barrier();
@@ -44,28 +49,27 @@ int main () {
   }
 
   while (1) {
-          __asm__ volatile("wfi;");
+    __asm__ volatile("wfi;");
   }
 
   return 0;
 }
 
 void __attribute__ ((noinline)) matrix_init(MA_TYPE * __restrict__ A, MB_TYPE * __restrict__ B, OUT_TYPE * __restrict__ C) {
-  for (int i = 0; i < M; i++) 
+
+  /*for (int i = 0; i < M; i++)
     for (int j = 0; j < N; j++){
       A[i*N+j] = A_mat[i*N+j];
+    }
 
-
-    } 
-      
-  for (int i = 0; i < N; i++) 
+  for (int i = 0; i < N; i++)
     for (int j = 0; j < P; j++){
       B[i*P+j] = B_mat[i*P+j];
-    }
-  for (int i = 0; i < M; i++) 
-    for (int j = 0; j < P; j++)  
+    }*/
+
+  for (int i = 0; i < M; i++)
+    for (int j = 0; j < P; j++)
       C[i*P+j] = 0;
-  
 }
 
 int __attribute ((noinline)) check_result(OUT_TYPE * __restrict__ result) {
@@ -80,7 +84,7 @@ int __attribute ((noinline)) check_result(OUT_TYPE * __restrict__ result) {
 
         printf("Error at index %d:\t refrence %f\t output %f\t error %.4f\n", i, ref[i], result[i], diff);
       #endif
-      
+
       }
 
       #ifdef PRINT_RESULTS
@@ -93,15 +97,48 @@ int __attribute ((noinline)) check_result(OUT_TYPE * __restrict__ result) {
 }
 
 void main_fn(int *retval){
-  
-  if (get_core_id() == 0)
-    matrix_init(matA, matB, matC);
- 
-  #ifndef FABRIC
-  synch_barrier();
-  #endif
-  
-  matMul(matA, matB, matC, M, N, P);
+
+  uint32_t gpio_val= 0x00000004;
+  int cycle_start=0;
+  int cycle_end=0;
+
+  //power loop
+  //while(1){
+    if (get_core_id() == 0)
+      matrix_init(matA, matB, matC);
+
+    #ifndef FABRIC
+    synch_barrier();
+    #endif
+
+    // Enable perf counter
+    if (get_core_id() == 0){
+      pulp_write32(0x10200020,0x00000000);
+        // Enable perf counter
+        /*perf_stop();
+        perf_reset();
+        cycle_start=cpu_perf_get(CSR_PCER_CYCLES);
+        perf_start();*/
+        pulp_write32(0x10200020,0x00000001);
+    }
+
+    matMul(A_mat, B_mat, matC, M, N, P);
+
+    if (get_core_id() == 0){
+      // perf counter off
+      /*perf_stop();
+      cycle_end= cpu_perf_get(CSR_PCER_CYCLES) - cycle_start;
+      pulp_write32(0x1C000300, cycle_end);*/
+      //(0x10200020,0x00000000);
+      if (check_result(matC)==0){
+        gpio_val ^= 0x00000004;
+        pulp_write32(GPIO2_PAD_A2_ADDR,gpio_val);
+      }else{
+        pulp_write32(GPIO2_PAD_A2_ADDR,0x00000000);
+        while(1){}
+      }
+    }
+  //} // while power
 
   #ifdef CHECK
   if (get_core_id() == 0)
