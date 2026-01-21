eea9871f6f367707fbff13d6cf5ecbaddd925c1b
Mattia Sinigaglia <mattia.sinigaglia5@unibo.it>
2026-01-21 09:32:26 +0100

    sw: upate fp16 PULP matmul - data in L1

--- software/par_matmul_fp16/par_matmul_fp16.c
+++ software/par_matmul_fp16/par_matmul_fp16.c
@@ -2,9 +2,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
+#include "gpio_v3.h"
 #include "./cluster_code.h"
 
-//#define FPGA_EMULATION
+#define PLIC_BASE 0x0C000000
+#define PLIC_CHECK PLIC_BASE + 0x201004
+//enable bits for sources 0-31
+#define PLIC_EN_BITS  PLIC_BASE + 0x2080
+
+
+#define ARCHI_GPIO_ADDR 0x1A105000
+#define GPIO2_PAD_A2_ADDR 0x1A10500C
+
+#define OUT 1
+#define IN  0
+
+uint32_t configure_gpio(uint32_t number, uint32_t direction){
+  uint32_t address;
+  uint32_t dir;
+  uint32_t gpioen;
+
+  //--- set GPIO
+  if(number < 32)
+  {
+    if (direction == IN)
+    {
+
+      address = ARCHI_GPIO_ADDR + GPIO_GPIOEN_OFFSET;
+
+      gpioen = pulp_read32(address);
+      //--- enable GPIO
+      //printf("GPIOEN RD: %x\n",gpioen);
+      gpioen |= (1 << number);
+      //printf("GPIOEN WR: %x\n",gpioen);
+      pulp_write32(address, gpioen);
+      //--- set direction
+      address = ARCHI_GPIO_ADDR + GPIO_PADDIR_OFFSET;
+      dir = pulp_read32(address);
+      //printf("GPIODIR RD: %x\n",dir);
+      dir |= (0 << number);
+      //printf("GPIODIR WR: %x\n",dir);
+      pulp_write32(address, dir);
+
+    }else if (direction == OUT){
+      //--- enable GPIO
+      address = ARCHI_GPIO_ADDR + GPIO_GPIOEN_OFFSET;
+      gpioen = pulp_read32(address);
+      gpioen |= (1 << number);
+      pulp_write32(address, gpioen);
+      /*printf("GPIO Enable Address %x  Val: %x\n\r",address, gpioen);
+      uart_wait_tx_done();*/
+      //--- set direction
+      dir=gpioen;
+      address = ARCHI_GPIO_ADDR + GPIO_PADDIR_OFFSET;
+      pulp_write32(address, dir);
+      /*printf("GPIO Direction OUT Address %x  Val: %x\n\r",address, dir);
+      uart_wait_tx_done();*/
+    }
+  }else{
+    if (direction == IN)
+    {
+      address = ARCHI_GPIO_ADDR + GPIO_GPIOEN_32_63_OFFSET;
+      gpioen = pulp_read32(address);
+      //--- enable GPIO
+      //printf("GPIOEN RD: %x\n",gpioen);
+      gpioen |= (1 << (number-32));
+      //printf("GPIOEN WR: %x\n",gpioen);
+      pulp_write32(address, gpioen);
+      //--- set direction
+      address = ARCHI_GPIO_ADDR + GPIO_PADDIR_32_63_OFFSET;
+      dir = pulp_read32(address);
+      //printf("GPIODIR RD: %x\n",dir);
+      dir |= (0 << (number-32));
+      //printf("GPIODIR WR: %x\n",dir);
+      pulp_write32(address, dir);
+    }else if (direction == OUT){
+      //--- enable GPIO
+      address = ARCHI_GPIO_ADDR + GPIO_GPIOEN_32_63_OFFSET;
+      gpioen = pulp_read32(address);
+      gpioen |= (1 << (number-32));
+      pulp_write32(address, gpioen);
+      //--- set direction
+      dir=gpioen;
+      address = ARCHI_GPIO_ADDR + GPIO_PADDIR_32_63_OFFSET;
+      pulp_write32(address, dir);
+    }
+  }
+
+  while(pulp_read32(address) != dir);
+
+}
+
+void set_gpio(uint32_t number, uint32_t value){
+  uint32_t value_wr;
+  uint32_t address;
+  if (number < 32)
+  {
+    address = ARCHI_GPIO_ADDR + GPIO_PADOUT_OFFSET;
+    value_wr = pulp_read32(address);
+    if (value == 1)
+    {
+      value_wr |= (1 << (number));
+    }else{
+      value_wr &= ~(1 << (number));
+    }
+    pulp_write32(address, value_wr);
+  }else{
+    address = ARCHI_GPIO_ADDR + GPIO_PADOUT_32_63_OFFSET;
+    value_wr = pulp_read32(address);
+    if (value == 1)
+    {
+      value_wr |= (1 << (number % 32));
+    }else{
+      value_wr &= ~(1 << (number % 32));
+    }
+    pulp_write32(address, value_wr);
+  }
+
+  while(pulp_read32(address) != value_wr);
+}
 
 int launch_cluster() {
 
@@ -15,8 +131,12 @@ int launch_cluster() {
   uint32_t mb_plic_id = 8;
   uint32_t plic_context = 0;
 
-  plic_set_priority(mb_plic_id, 0x1);
-  plic_set_ie(plic_context, mb_plic_id, 0x1);
+  pulp_write32(PLIC_BASE+mb_plic_id*4, 1); // set mb_plic_id interrupt priority to 1
+
+  pulp_write32(PLIC_EN_BITS+(((int)(mb_plic_id/32))*4), 1<<(mb_plic_id%32)); //enable interrupt
+
+  // Clear IRQS Register
+  pulp_write32(0x10403000 + 0x18, 0x7);
 
   // Flush R FIFO
   pulp_write32(0x10402000 + 0x24, (0x1 << 1));
@@ -30,46 +150,59 @@ int launch_cluster() {
 
   init_cluster(BOOT_ADDR);
 
-  while (plic_claim_msg(plic_context) != mb_plic_id) {
+  while (pulp_read32(PLIC_CHECK)!= mb_plic_id) {
    asm volatile ("wfi");
   }
+
+  // Core in sleep
+  /*while (1) {
+   asm volatile ("wfi");
+  }*/
 
   // Read irq status
   uint32_t mb_irqs = pulp_read32(0x10403000 + 0x18);
   uint32_t msg = -1;
   if ((mb_irqs & 0b111) == 0b001) {
-    // Clear interrupt in mb
-    pulp_write32(0x10403000 + 0x18, 0b001);
+
+    // Read the message
     msg = pulp_read32(0x10402000 + 0x04);
+
   }
 
   // Complete irq
-  plic_complete_msg(plic_context, mb_plic_id);
+  pulp_write32(PLIC_CHECK,mb_plic_id);
 
   return msg;
 }
 
 int main(int argc, char const *argv[]) {
 
-  unsigned int msg;
+  unsigned int msg = -1;
   int retval = 0;
+  uint32_t mb_plic_id = 8;
 
-  printf("Test init\r\n");
-  uart_wait_tx_done();
+  uint32_t gpio_val= 0x00000004;
+
+  // Config Pad GPIO 2
+  //alsaqr_periph_padframe_periphs_a_02_mux_set(2);
+
+  // Set GPIO 0 OUT
+  //configure_gpio(2, OUT);
+  //set_gpio(2, gpio_val);
+
+  // Apply this on the chip if needed to gate/ungate the cluster
+  //clock_gating_cluster();
+
   msg = launch_cluster();
 
-  if(msg == 0)
-    printf("Cl ok\n");
-  else
-    printf("Cl error! %x\n", msg);
+  //clock_gating_cluster();
 
-  uart_wait_tx_done();
+  if(msg == 0){
+    //printf("Success!! \r\n");
+  }
+  else{
+    //printf("Fail!!\r\n");
+  }
 
-  printf("UART BASE: %X\n", UART_BASE_ADDR);
-  uart_wait_tx_done();
-
-  printf("Test Done\r\n");
-  uart_wait_tx_done();
-
-  return msg;
+  return (msg != 0);
 }
---
+++ software/par_matmul_fp16/stimuli/data_generator.py
@@ -0,0 +1,213 @@
+
+#!/bin/python3
+
+# Run it by:
+# ./data_generator.py --M=m --N=n --P=p --float_type=fmt --MAC_flag=true
+# The script will generate three floating-point matrices fo format fmt (FP32/FP16/FP16ALT):
+#
+
+
+import os
+import argparse
+import sys
+
+import torch
+from torch import nn
+
+
+def relative_absolute_error(true, pred):
+    true_mean = torch.mean(true)
+    squared_error_num = torch.sum(torch.abs(true - pred))
+    squared_error_den = torch.sum(torch.abs(true - true_mean))
+    rae_loss = squared_error_num / squared_error_den
+    return rae_loss
+
+
+def mean_squared_error(true, pred):
+    squared_error = torch.square(true - pred)
+    sum_squared_error = torch.sum(squared_error)
+    size = true.size(dim=0) * true.size(dim=1)
+    mse_loss = sum_squared_error / size
+    return mse_loss
+
+
+def matrix_init(IN, dt):
+    temp = torch.zeros((IN.shape[0], IN.shape[1]), dtype=dt)
+    # iterate through rows of IN
+    for i in range(IN.shape[0]):
+        # iterate through columns of IN
+        for j in range(IN.shape[1]):
+            temp[i][j] = IN[i][j]
+    return temp
+
+
+def error_metric(ref, res):
+
+    # calculate manually because metrics doesn't supprt bfloat16
+    d = ref - res
+    mse_f = torch.mean(d**2)
+    mae_f = torch.mean(abs(d))
+    rmse_f = torch.sqrt(mse_f)
+    r2_f = 1-(torch.sum(d**2)/torch.sum((ref-torch.mean(ref))**2))
+    print("Results of metrics:")
+    print("MAE:",mae_f.item())
+    print("MSE:", mse_f.item())
+    print("RMSE:", rmse_f.item())
+    print("R-Squared:", r2_f.item())
+    rae = relative_absolute_error(ref, res)
+    print("RAE is", rae.item())
+
+
+def matrix_mult(Xs, Ys, dt, mac_flag, cast_flag, cast_to):
+    Rs = torch.zeros((Xs.shape[0], Ys.shape[1]), dtype=dt)
+    # iterate through rows of X
+    for i in range(Xs.shape[0]):
+        # iterate through columns of Y
+        for j in range(Ys.shape[1]):
+            temp = torch.tensor([0], dtype=dt)
+            # iterate through rows of Y
+            for k in range(Ys.shape[0]):
+                a = Xs[i][k]
+                b = Ys[k][j]
+                if cast_flag == "true":
+                    if cast_to == "FP16":
+                        a = a.type(torch.float16)
+                        b = b.type(torch.float16)
+                    elif cast_to == "FP16ALT":
+                        a = a.type(torch.bfloat16)
+                        b = b.type(torch.bfloat16)
+                if mac_flag == "true":
+                    a = a.type(torch.float32)
+                    b = b.type(torch.float32)
+                    temp = temp.type(torch.float32)
+                temp += a * b
+                if mac_flag == "true":
+                    temp = temp.type(dt)
+
+            Rs[i][j] = temp
+    return Rs
+
+
+def write_matrix(matrix_to_write, name, file_pointer, float_type):
+    matrix_string = ''
+    sz0 = matrix_to_write.size()[0]
+    sz1 = matrix_to_write.size()[1]
+    if 'ref' in name:
+        file_pointer.write("L1_GLOBAL_DATA OUT_TYPE %s[] = {" % name)
+    elif 'A_mat' in name:
+        file_pointer.write("L1_GLOBAL_DATA MA_TYPE %s[] = {" % name)
+    else:
+        file_pointer.write("L1_GLOBAL_DATA MB_TYPE %s[] = {" % name)
+    if float_type == torch.float32:
+        name = ")"
+    elif float_type == torch.float16:
+        name = ", dtype=torch.float16)"
+    elif float_type == torch.bfloat16:
+        name = ", dtype=torch.bfloat16)"
+    for i in range(sz0):
+        for j in range(sz1):
+            matrix_string += str(matrix_to_write[i][j].item()).replace('tensor(', '').replace(name, '')
+            matrix_string += ', '
+    file_pointer.write("%s" % matrix_string)
+    file_pointer.write("};\n")
+
+
+def get_inital_config():
+    # get arguments  and data format
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--M')
+    parser.add_argument('--N')
+    parser.add_argument('--P')
+
+    parser.add_argument('--MAC_flag', default="true")
+    parser.add_argument('--float_type', default='FP32')
+    args = parser.parse_args()
+
+    M = int(args.M)
+    N = int(args.N)
+    P = int(args.P)
+    mac_flag = str(args.MAC_flag)
+    bits = args.float_type.split(",")
+    return M, N, P, bits, mac_flag
+
+
+def select_dtypes(user_dtypes, num_param):
+    types_dict = {
+        "FP32": torch.float32,
+        "FP16": torch.float16,
+        "FP16ALT": torch.bfloat16
+    }
+    dtypes = []
+    if len(user_dtypes) == 1:
+        for i in range(num_param):
+            dtypes.append(types_dict[user_dtypes[0]])
+    elif len(user_dtypes) == num_param:
+        for i in range(num_param):
+            dtypes.append(types_dict[user_dtypes[i]])
+    else:
+        for i in range(len(user_dtypes)):
+            dtypes.append(types_dict[user_dtypes[i]])
+        if 'FP32' in user_dtypes:
+            for i in range(len(user_dtypes), num_param):
+                dtypes.append(types_dict["FP32"])
+        elif 'FP16' in user_dtypes:
+            for i in range(len(user_dtypes), num_param):
+                dtypes.append(types_dict["FP16"])
+        else:
+            for i in range(len(user_dtypes), num_param):
+                dtypes.append(types_dict["FP16ALT"])
+    return dtypes
+
+def check_cast(datatypes):
+    result = len(set(datatypes)) == 1
+    if result : #All Elements in List are Equal
+        return "false"
+    else: #All Elements in List are Not Equal
+        if torch.float32 in datatypes:
+            return "false"
+        else:
+            return "true"
+
+def save_data_into_hfile(M, N, P, A_mat, B_mat, res):
+    # Generate header file
+    f = open('data.h', 'w')
+    f.write('\
+#define M %s\n\
+#define N %s\n\
+#define P %s\n\n' % (M, N, P))
+    write_matrix(A_mat, 'A_mat', f, A_mat.dtype)
+    write_matrix(B_mat, 'B_mat', f, B_mat.dtype)
+    write_matrix(res, 'ref', f, res.dtype)
+
+    f.close()
+
+
+def main():
+    M, N, P, bits, mac_flag = get_inital_config()
+
+    # Create reference matrices
+    A_ref = torch.randn((M, N), dtype=torch.float32)
+    B_ref = torch.randn((N, P), dtype=torch.float32)
+
+    # calculate reference output
+    ref = matrix_mult(Xs=A_ref, Ys=B_ref, dt=torch.float32, mac_flag=mac_flag, cast_flag="false",cast_to="false")
+
+    # set the data types based on the parser input
+    datatypes = select_dtypes(bits, 3)
+
+    cast_flag = check_cast(datatypes[0:2])
+    cast_to = "FP16ALT"
+    A_mat = matrix_init(A_ref, dt=datatypes[0])
+    B_mat = matrix_init(B_ref, dt=datatypes[1])
+
+    res = matrix_mult(Xs=A_mat, Ys=B_mat, dt=datatypes[2], mac_flag=mac_flag, cast_flag=cast_flag, cast_to = cast_to)
+
+    error_metric(ref, res)
+    save_data_into_hfile(M, N, P, A_mat, B_mat, res)
+    print("############################## Done! ###################################")
+    return None
+
+
+if __name__ == "__main__":
+    main()
+    pass
